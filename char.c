ceed2af10b1cfca0143dc5cc92a96a34f109f986
nikitatorop <torop.nikita1@gmail.com>
2022-05-25 09:38:59 +0300

    Add logic of changing the original value with pointers

--- char.c
+++ char.c
@@ -8,8 +8,15 @@ int main(void) {
     char str[20] = "Hello!";
     char *p = str;
     char *first = &str[0];
+    char first_symbol = *first;
 
     printf("Char str: %s\n", str);
     printf("Address of char str: %p\n", p);
-    printf("Address of the first symbol of str: %p", first);
+    printf("Address of the first symbol of str: %p\n", first);
+    printf("Copy of the first symbol of str: %c\n", first_symbol);
+
+    first_symbol = 'A';
+
+    printf("Copy of the first symbol of str: %c\n", first_symbol);
+    printf("Char str that hasn't changed: %s\n", str);
 }
